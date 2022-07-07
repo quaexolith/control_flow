8f17a5dc3f8787940957290547969be867017a48
Quaexolith <quaexolith@gmail.com>
2022-07-07 06:40:23 -0400

    Control flow with loops and labels with break and continue

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ Using too many `else if`s can clutter the code. Rust recommends using the `match
 Because `if` is an expression, we can use it on the right side of a `let` statement to assign the outcome to a variable.
 
 Code blocks evaluate to the last expression in them. The value of the `if` in a `let` statement depends on which block of code executes. The values that have the potential to be results from each arm of the `if` must be the same type.
+
+# Repetition with Loops
+Rust has 3 kinds of loops: `loop`, `while`, and `for`.
+
+## Repeating code with loop
+Use `break` or `continue` to interrupt loop execution.
+
+## Returning Values from Loops
+Add the value to return after the `break`;
+
+## Loop Labels to Disambiguate Between Multiple Loops
+`break` and `continue` apply to the innermost loop. You can optionally specify a **loop label** that can be used with `break` or `continue` to specify those keywords apply to the labelled loop instead of the innermost loop.
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ fn main() {
     check_divisibility(6);
 
     let_assignment();
+
+    loop_break_with_counter();
+
+    loop_with_label();
 }
 
 fn check_divisibility(number: i32) {
@@ -35,4 +39,40 @@ fn let_assignment() {
     let number = if condition { 5 } else { 6 };
 
     println!("The value of let_assignment is: {number}");
+}
+
+fn loop_break_with_counter() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn loop_with_label() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
